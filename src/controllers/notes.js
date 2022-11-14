20f3899e6859dc8cf2cc064713f7a605443d9d44
Ian Maulana <ian_maulana@yahoo.com>
2022-11-15 00:22:22 +0700

    Create controller

---
+++ src/controllers/notes.js
@@ -0,0 +1,50 @@
+/**
+ * @desc Fetch all notes
+ * @route GET /api/v1/notes
+ * @acces Private
+ */
+exports.getNotes = (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all notes' });
+};
+
+/**
+ * @desc Fetch note by id
+ * @route GET /api/v1/notes/:id
+ * @acces Private
+ */
+exports.getNote = (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show note with ${req.params.id}` });
+};
+
+/**
+ * @desc Create new note
+ * @route POST /api/v1/notes
+ * @acces Private
+ */
+exports.createNote = (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new note' });
+};
+
+/**
+ * @desc Update note by id
+ * @route PUT /api/v1/notes/:id
+ * @acces Private
+ */
+exports.updateNote = (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update note with ${req.params.id}` });
+};
+
+/**
+ * @desc Delete note by id
+ * @route DELETE /api/v1/notes/:id
+ * @acces Private
+ */
+exports.deleteNote = (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete note with ${req.params.id}` });
+};
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,28 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getNotes,
+  createNote,
+  getNote,
+  updateNote,
+  deleteNote,
+} = require('../controllers/notes');
 
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all notes' });
-});
-
-router.get('/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Show note by ${req.params.id}` });
-});
-
-router.post('/', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create new note' });
-});
-
-router.put('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update note with ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete note with ${req.params.id}` });
-});
+router.route('/').get(getNotes).post(createNote);
+router.route('/:id').get(getNote).put(updateNote).delete(deleteNote);
 
 module.exports = router;
